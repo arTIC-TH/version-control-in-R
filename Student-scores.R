b8b051ffff0c825ad549daba534eb135b4bd73b4
arTIC-TH <scriverat@gmail.com>
2021-02-26 18:04:29 +0100

    parte inicial

--- Student-scores.R
+++ Student-scores.R
@@ -1,4 +1,6 @@
-Create a character vector called Names to store the names of the 5 students
+## Create a vector called scores to store the marks of 5 students
+Scores <- c(65,68,70,70,71)
+## Create a character vector called Names to store the names of the 5 students
 Names <- c("Tayo", "Chance", "Seyi", "Louis", "Phil")
 length(Names) #to confirm if we have 5 entries
 ## Create a character vector called Gender that stores the gender of the 5 students
